cc956c07f49d651aeb6aceee5fd341d70f83b42e
zubyul <yulia.zubak@mail.utoronto.ca>
2023-12-14 16:22:00 -0600

    adds space for image but images don't work

--- experiment1/client/src/Game.jsx
+++ experiment1/client/src/Game.jsx
@@ -2,7 +2,8 @@ import { useGame } from "@empirica/core/player/classic/react";
 
 import React from "react";
 import { Chat } from "./components/Chat"
-import { Profile } from "./components/Profile";
+import { Profile } from "./components/Profile"
+import { World } from "./components/World";
 
 export function Game() {
   const game = useGame();
@@ -10,6 +11,9 @@ export function Game() {
 
   return (
     <div className="h-full w-full flex">
+      <div className="world-container" style={{ flex: 1 }}>
+        <World />
+      </div>
       <div className="h-full w-full flex flex-col">
         <Profile />
         <div className="h-full flex items-center justify-center">
---
+++ experiment1/client/src/components/World.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export function World() {
+  return <img src="/image.png" alt="Squirrel 1" />;
+}
+
