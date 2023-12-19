360f17373b8e0a7287c63b5758f5d88fdfd8a910
zubyul <yulia.zubak@mail.utoronto.ca>
2023-12-19 17:33:42 -0600

    50/50 chance animal or blank space

--- experiment1/client/src/components/World.jsx
+++ experiment1/client/src/components/World.jsx
@@ -3,13 +3,18 @@ import React from "react";
 export function World() {
   const generateRandomEmoji = () => { 
     const isRabbit = Math.random() < 0.5;
-    return isRabbit ? '🐇' : '🐿️';
+    const isSpace = Math.random() < 0.5;
+    if (isSpace) {
+      return '⠀⠀';
+    } else {
+      return isRabbit ? '🐇' : '🐿️';
+    }
   };
 
-
+  const numberOfEmojis = Math.floor(Math.random() * 8) + 1
   return (
-    <div  style={{ backgroundImage: 'url("/freepik.png")', backgroundColor: '#268b07', width: '90%', height: '90%', borderRadius: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      {[...Array(8)].map((_, index) => (
+    <div  style={{ backgroundImage: 'url("/freepik.png")', backgroundColor: '#268b07', width: '90%', height: '90%', borderRadius: '20px', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+      {[...Array(numberOfEmojis)].map((_, index) => (
         <span key={index} style={{ fontSize: '40px' }}>
           {generateRandomEmoji()}
         </span>
