eae1aec5221023ff566a661c008cbc8ad0a82017
Robert Hawkins <hawkrobe@gmail.com>
2023-12-21 05:35:13 -0600

    revise client-side emoji creation

--- experiment1/client/src/components/World.jsx
+++ experiment1/client/src/components/World.jsx
@@ -1,47 +1,32 @@
 import React from "react";
 import { useGame, usePlayer } from "@empirica/core/player/classic/react";
+import _ from "lodash";
 
 export function World() {
   const game = useGame();
   const player = usePlayer();
 
+  // turn critter distribution into emoji arrays
   const critterDistribution = player.get('critters')
-  const emojiMapping = {nRabbits: '🐇',nSquirrels: '🐿️'};
+  const rabbits = _.repeat('🐇 ', critterDistribution['nRabbits']).split(' ')
+  const squirrels = _.repeat('🐿️ ', critterDistribution['nSquirrels']).split(' ')
 
+  // create spaces with roughly 50% probability
+  const nSpaces = 1/2 * _.sum(_.values(critterDistribution))
+  const spaces = _.repeat('\u00A0 \u00A0 \u00A0 \u00A0', nSpaces)
 
-  //console.log(typeof critterDistribution)
-
-const emojiArray = (critterDistribution) => {
-  const emojiArray = [];
-
-  for (const key in critterDistribution) {
-    if (critterDistribution.hasOwnProperty(key) && emojiMapping[key]) {
-      const emoji = emojiMapping[key];
-      const count = critterDistribution[key];
-
-      for (let i = 0; i < count; i++) {
-        emojiArray.push(emoji);
-        if (Math.random() < 0.5) {
-          emojiArray.push('\u00A0 \u00A0 \u00A0 \u00A0');
-        }
-      }
-    }
-  }
-   for (let i = emojiArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [emojiArray[i], emojiArray[j]] = [emojiArray[j], emojiArray[i]];
-  }
-
-  return emojiArray;
-};
-
-
+  // scramble the order
+  const emojiArray = _.shuffle(_.concat(rabbits, squirrels, spaces));
 
   return (
-    <div  style={{ backgroundImage: 'url("/freepik.png")', backgroundColor: '#268b07', width: '90%', height: '90%', borderRadius: '20px', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{
+           backgroundImage: 'url("/freepik.png")', backgroundColor: '#268b07',
+           width: '90%', height: '90%',
+           borderRadius: '20px', display: 'flex', flexWrap: 'wrap',
+           justifyContent: 'center', alignItems: 'center'}}>
       {[...Array(1)].map((_, index) => (
-        <span key={index} style={{ fontSize: '40px' }}>
-          {emojiArray(critterDistribution)}
+        <span key={index} style={{ fontSize: '70px' }}>
+          {emojiArray}
         </span>
       ))}
     </div>
