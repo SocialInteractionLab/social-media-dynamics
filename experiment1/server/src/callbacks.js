515b8c0bc033249403a9cdb9dcfa480074bc4b94
zubyul <yulia.zubak@mail.utoronto.ca>
2023-12-21 16:46:59 -0600

    create a distribution of critter ratios, sample from it, and hide some

--- experiment1/server/src/callbacks.js
+++ experiment1/server/src/callbacks.js
@@ -4,18 +4,62 @@ import _ from "lodash";
 
 
 Empirica.onGameStart(({ game }) => {
-  const critters = {nRabbits: 3, nSquirrels: 1}
-  const rabbits = _.repeat('🐇 ', critters['nRabbits']).split(' ')
-  const squirrels = _.repeat('🐿️ ', critters['nSquirrels']).split(' ')
+
+
+// generate a normal distribution between 0-8 with a mean of 4 
+function generateRatioDistribution(mean, stdDev, range) {
+  const values = [];
+
+  for (let i = 0; i < range.length; i++) {
+    const ratio = range[i];
+    const probability = Math.exp(-(Math.pow(ratio - mean, 2) / (2 * Math.pow(stdDev, 2)))) / (stdDev * Math.sqrt(2 * Math.PI));
+    values.push({ x: ratio, y: probability });
+  }
+
+  return values;
+}
+
+const meanRatio = 4;  
+const stdDevRatio = 2;  
+
+const rangeRatio = Array.from({ length: 9 }, (_, i) => i); 
+
+// Generate distribution for the ratio of squirrels to rabbits
+const distributionRatio = generateRatioDistribution(meanRatio, stdDevRatio, rangeRatio);
+
+function sampleRandomRatio(distribution) {
+  const rand = Math.random();
+  let cumulativeProbability = 0;
+
+  for (const entry of distribution) {
+    cumulativeProbability += entry.y;
+    if (rand <= cumulativeProbability) {
+      return entry.x;
+    }
+  }
+
+  // In case of rounding errors, return the last value
+  return distribution[distribution.length - 1].x;
+}
+
+
+game.players.forEach((player,i) => {
+  const nrabbits = Array.from({ length: 1 }, () => sampleRandomRatio(distributionRatio))[0];
+  const nsquirrels = 8 - nrabbits;
+
+const rabbits = _.repeat('🐇 ', nrabbits).split(' ');
+const squirrels = _.repeat('🐿️ ', nsquirrels).split(' ');
+
+console.log('nR:', nrabbits);
+  console.log('nS:', nsquirrels);
 
  // create spaces with roughly 50% probability
-  const nSpaces = 1/2 * _.sum(_.values(critters))
+  const nSpaces = 1/2 * [nrabbits+nsquirrels]
   const spaces = _.repeat('\u00A0 \u00A0 \u00A0 \u00A0', nSpaces)
 
   // scramble the order
   const emojiArray = _.shuffle(_.concat(rabbits, squirrels, spaces));
 
-  game.players.forEach((player, i) => {
     player.set("name", "player " + i);
     player.set("emojiArray", emojiArray)
   });
