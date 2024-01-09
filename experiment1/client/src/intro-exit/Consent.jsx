b7cc33b232da143803214d9da7c2730934b13fc6
zubyul <yulia.zubak@mail.utoronto.ca>
2024-01-09 14:08:02 -0600

    reworked consent agreement

--- experiment1/client/src/intro-exit/Consent.jsx
+++ experiment1/client/src/intro-exit/Consent.jsx
@@ -2,38 +2,54 @@ import React from "react";
 import { Button } from "../components/Button";
 
 export function Consent({ next }) {
-  return ( <div className="flex items-center justify-center w-screen"><div className="w-1/2">
-    <div className="mt-3 sm:mt-5 p-20">
-      <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Consent
-      </h3>
-      <div>
+  return (
+    <div className="flex items-center justify-center w-screen">
+      <div className="w-1/2 mt-3 sm:mt-5 p-20">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Consent</h3>
         <div className="instructions">
           <div className="smallimage">
-            <center><img width="300px" src="./madison.png" /></center>
+            <center>
+              <img width="300px" src="./madison.png" alt="Madison" />
+            </center>
           </div>
           <p>
-            If you agree to take part in the research, you will play a series of communication 
-            games with other participants: you will be describing the amount of animals you see, 
-            with the goal of all agreeing on how many there are. <br/>
-            <br/>This study will take approximately twenty minutes.
+            Please read this consent agreement carefully before deciding whether to
+            participate in this experiment. 
+          </p><br/>
+          <p>
+            <strong>What you will do in this research:</strong> You will play a series of communication 
+            games with other participants: describing the amount of animals you see, 
+            with the goal of all agreeing on how many there are.
           </p> <br/>
-          <p className="block-text" id="legal">
-            You are participating in a study being performed by cognitive scientists in the 
-            University of Wisconsin Madison Psychology Department. 
-            If you have questions about this research, please contact us 
-            at <a href="mailto://@.com."> @.com</a>.
-           <br/> <br/> 
-           You must be at least 18 years old to participate. 
-           Your participation in this research is voluntary. 
-           You may decline to answer any or all of the following questions. 
-           You may decline further participation, at any time, without adverse consequences. 
-           Your anonymity is assured. </p><br/><br/>
+          <p>
+            <strong>Time required:</strong> This study will take approximately twenty minutes.
+          </p><br/>
+          <p>
+            <strong>Purpose of the research:</strong> The purpose is to understand how an agreement is reached in a networked community.
+          </p><br/>
+          <p>
+            <strong>Risks:</strong> There are no anticipated risks associated with participating in this study. The effects should be comparable to viewing a computer monitor and using a mouse.
+          </p><br/>
+          <p>
+            <strong>Compensation:</strong> You will receive x for completing the experiment and a performance bonus of up to x.
+          </p><br/>
+          <p>
+            <strong>Confidentiality:</strong> Your participation will remain confidential. No personally identifiable information will be collected. Your anonymous data may be shared with other researchers and used in future projects.
+          </p><br/>
+          <p>
+            <strong>Participation and withdrawal:</strong> Your participation is completely voluntary. You may refuse to participate or choose to withdraw at any time without penalty or loss of benefits.
+          </p><br/>
+          <p>
+            <strong>How to contact the researcher:</strong> If you have questions or concerns, please contact <a href="mailto:yzubak@wisc.edu">yzubak@wisc.edu</a>.
+          </p><br/>
+          <p>
+            <strong>Whom to contact about your rights in this research:</strong> For questions, concerns, suggestions, or complaints, contact the University of Wisconsin-Madison Human Research Protection Program at 608-890-4399 or <a href="mailto:compliance@research.wisc.edu">compliance@research.wisc.edu</a>.
+          </p><br/>
         </div>
+        <Button handleClick={next} autoFocus>
+          <p>Next</p>
+        </Button>
       </div>
-      <Button handleClick={next} autoFocus>
-        <p>Next</p>
-      </Button>
-    </div> </div> </div>
+    </div>
   );
 }
