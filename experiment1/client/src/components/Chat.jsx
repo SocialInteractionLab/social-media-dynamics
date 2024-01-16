0681b773f448e4cff3f4d1d4f23df6a2fd901122
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-01-16 15:59:44 -0800

    persistence for chat messages, appends whole log to game

--- experiment1/client/src/components/Chat.jsx
+++ experiment1/client/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import {usePlayer, useStage, useRound } from "@empirica/core/player/classic/react";
+import {usePlayer, useStage, useRound, useGame } from "@empirica/core/player/classic/react";
 import { Loading } from "@empirica/core/player/react";
 import React, {useState, useRef, useEffect } from "react";
 import { Opinion } from "./Opinion";
@@ -15,11 +15,11 @@ import h from '/avatar8.png';
 //import RangeSlider from 'react-range-slider-input';
 //import 'react-range-slider-input/dist/style.css';
 
-
 export function Chat({ scope, attribute, loading}) {
     const player = usePlayer();
     const round = useRound();
     const stage = useStage();
+    const game = useGame();
 
     if (!scope || !player) {
         return <LoadingComp />;
@@ -36,6 +36,9 @@ export function Chat({ scope, attribute, loading}) {
                 avatar: player.get("avatar"),
             },
         });
+        const playerStageData = scope.getAttribute(attribute)?.items || [];
+        console.log(playerStageData);
+        game.set("messages", playerStageData);
     };
 
     let msgs = scope.getAttribute(attribute)?.items || [];
