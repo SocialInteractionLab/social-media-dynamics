1002a2faa10031425efeec67844b550dc1647904
Robert Hawkins <hawkrobe@gmail.com>
2024-01-18 22:54:44 +0000

    fix html tags

--- experiment1/client/src/intro-exit/Introduction.jsx
+++ experiment1/client/src/intro-exit/Introduction.jsx
@@ -13,7 +13,7 @@ export function Introduction({ next }) {
        <b> Please read the instructions carefully. There will be a quiz. You cannot continue to the game if you do not pass the quiz.</b> <br/>
         <br/>        
           In this game, you are part of a team of 6-8 players. You are all <b>neighbors </b>in the same area, using a semi-anonymous messaging app to talk to each other. 
-          While each individual message is only sent to and received by one person, everyone is talking to multiple people. At the current stage of this experiment, you are not exchanging messages, but <b>leaving a message for someone else to read.<b><br />
+          While each individual message is only sent to and received by one person, everyone is talking to multiple people. At the current stage of this experiment, you are not exchanging messages, but <b>leaving a message for someone else to read.</b><br />
           <br/>Picture it like this, with each line being an interaction:
 
           <div className="network" style={{ margin: "20px" }}>
@@ -37,26 +37,31 @@ export function Introduction({ next }) {
         </div> <br/>
          <h3 className="text-lg leading-6 font-medium text-gray-900">
             Interface
-          </h3> 
-                     <div className="mt-2 mb-6">
+         </h3>
+         <div className="mt-2 mb-6">
+	     <p className="text-sm text-gray-700">
+		 <br/>
+		 Everyone on the team has a single  <b>window</b> they're looking out of, which you can see on the left of the image below.
+		 You will only be seeing <b>one set of critters</b> that will stay the same throughout the entire study.
+		 A new "round" (typing a message to a new neighbor) does not signal a new day or new animals. 
+		 <br/>
+		 <div className="window" style={{ margin: "20px" }}>
+		     <center><img width="700px" src="./interaction.png" /></center>
+		 </div>
 
-        <p className="text-sm text-gray-700">
-          <br/>Everyone on the team has a single  <b>window</b> they're looking out of, which you can see on the left of the image below. At the current stage of this experiment, you will only be getting <b>one set of critters</>- the page reloading does not signal a new day (for now!)
-          <br/><div className="window" style={{ margin: "20px" }}>
-            <center><img width="700px" src="./interaction.png" /></center>
-          </div>
+		 You're going to share what you think with your neighbor using the <b>messaging app</b>, seen to the right of the image.<br/> <br/>
 
-          You're going to share what you think with your neighbor using the <b>messaging app</b>, seen to the right of the image.<br /> <br/>
+		 Following this, you will read someone else's thoughts and make a judgment of the ratio of rabbits to squirrels using the <b>slider</b>. <br/> <br/> 
+		 The <b>slider</b> measures the proportion of the population as a percentage <b> of rabbits</b>.
+		 So if you think the population is half (or 50%) rabbits, you would put the slider in the middle.
+		 If you think it's all squirrels (0% rabbits) you would put it at the leftmost end. 
+		 <br/>
+		 <div className="slider" style={{ margin: "20px" }}>
+		     <center><img width="300px" src="./slider.png" /></center>
+		 </div>
+								    This process will repeat several times as you continue sharing with your neighbors how many rabbits and squirrels you think there are. 
 
-          Following this, you will read someone else's thoughts and make a judgment of the ratio of rabbits to squirrels using the <b>slider</b>. <br/> <br/> 
-          The <b>slider</b> measures the proportion of the population as a percentage <b> of rabbits</b>. So if you think the population is half (or 50%) rabbits, you would put the slider in the middle. If you think it's all squirrels (0% rabbits) you would put it at the leftmost end. 
-          <br/><div className="slider" style={{ margin: "20px" }}>
-            <center><img width="300px" src="./slider.png" /></center>
-          </div>
-
-          This process will repeat several times as you continue sharing with your neighbors how many rabbits and squirrels you think there are. 
-
-           <br/> <br/>You can be informative or uninformative, sharing as much or as little information as you like, with a caveat!<br />
+           <br/> <br/>You can be informative or uninformative, sharing as much or as little information as you like, with a caveat!<br/>
           <br/><b>If, by the end of the game, all of you are in agreement as to the ratio of critters, you will receive a bonus. The base pay for this experiment is 15$/hr. </b>
           On the next page is a quiz to test your knowledge of the UI and game!
         </p> 
