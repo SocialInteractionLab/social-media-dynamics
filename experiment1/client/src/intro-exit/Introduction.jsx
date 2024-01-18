c1ef910ebb0ee0779191803a621d0aaeb9e53241
zubyul <yulia.zubak@mail.utoronto.ca>
2024-01-18 15:21:05 -0600

    clarified intro

--- experiment1/client/src/intro-exit/Introduction.jsx
+++ experiment1/client/src/intro-exit/Introduction.jsx
@@ -13,7 +13,7 @@ export function Introduction({ next }) {
        <b> Please read the instructions carefully. There will be a quiz. You cannot continue to the game if you do not pass the quiz.</b> <br/>
         <br/>        
           In this game, you are part of a team of 6-8 players. You are all <b>neighbors </b>in the same area, using a semi-anonymous messaging app to talk to each other. 
-          While each individual message is only sent to and received by one person, everyone is talking to multiple people. At the current stage of this experiment, you are not exchanging messages, but leaving a message for someone else to read.<br />
+          While each individual message is only sent to and received by one person, everyone is talking to multiple people. At the current stage of this experiment, you are <b>not exchanging messages</b>, but leaving a message for someone else to read.<br />
           <br/>Picture it like this, with each line being an interaction:
 
           <div className="network" style={{ margin: "20px" }}>
@@ -41,7 +41,7 @@ export function Introduction({ next }) {
                      <div className="mt-2 mb-6">
 
         <p className="text-sm text-gray-700">
-          <br/>Everyone on the team has a single  <b>window</b> they're looking out of, which you can see on the left of the image below. At the current stage of this experiment, you will only be getting one set of critters- the page reloading does not signal a new day (for now!)
+          <br/>Everyone on the team has a single  <b>window</b> they're looking out of, which you can see on the left of the image below. At the current stage of this experiment, you will only be getting <b>one set of critters</b>- the page reloading does not signal a new day (for now!) 
           <br/><div className="window" style={{ margin: "20px" }}>
             <center><img width="700px" src="./interaction.png" /></center>
           </div>
@@ -57,7 +57,7 @@ export function Introduction({ next }) {
           This process will repeat several times as you continue sharing with your neighbors how many rabbits and squirrels you think there are. 
 
            <br/> <br/>You can be informative or uninformative, sharing as much or as little information as you like, with a caveat!<br />
-          <br/><b>If, by the end of the game, all of you are in agreement as to the ratio of critters, you will receive a bonus. The base pay for this experiment is 15$/hr. </b>
+          <br/><b>If, by the end of the game, all of you are in agreement as to the ratio of critters in the neighborhood, you will receive a bonus. The base pay for this experiment is 15$/hr. </b>
           On the next page is a quiz to test your knowledge of the UI and game!
         </p> 
       </div>
