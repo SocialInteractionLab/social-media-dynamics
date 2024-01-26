76b512cd47d5f07dff95e7631d12732ecdd02d0d
zubyul <yulia.zubak@mail.utoronto.ca>
2024-01-26 15:17:02 -0600

    all chat condition - unlocked chat and improved slider

---
+++ experiment 3 - open chat/client/src/intro-exit/Consent.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Button } from "../components/Button";
+
+export function Consent({ next }) {
+  return (
+    <div className="flex items-center justify-center w-screen">
+      <div className="w-1/2 mt-3 sm:mt-5 p-20">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Consent</h3>
+        <div className="instructions">
+          <div className="smallimage">
+            <center>
+              <img width="300px" src="./madison.png" alt="Madison" />
+            </center>
+          </div>
+          <p>
+            Please read this consent agreement carefully before deciding whether to
+            participate in this experiment. 
+          </p><br/>
+          <p>
+            <strong>What you will do in this research:</strong> You will play a series of communication 
+            games with other participants: describing the amount of animals you see, 
+            with the goal of all agreeing on how relatively many there are.
+          </p> <br/>
+          <p>
+            <strong>Time required:</strong> This study will take approximately twenty minutes.
+          </p><br/>
+          <p>
+            <strong>Purpose of the research:</strong> The purpose is to understand how an agreement is reached in a networked community.
+          </p><br/>
+          <p>
+            <strong>Risks:</strong> There are no anticipated risks associated with participating in this study. The effects should be comparable to viewing a computer monitor and using a mouse for the duration of the experiment.
+          </p><br/>
+          <p>
+            <strong>Compensation:</strong> You will receive x for completing the experiment and a performance bonus of up to x.
+          </p><br/>
+          <p>
+            <strong>Confidentiality:</strong> Your participation in this study will remain confidential. No personally identifiable information will be collected. Your anonymous data may be shared with other researchers and used in future projects.
+          </p><br/>
+          <p>
+            <strong>Participation and withdrawal:</strong> Your participation in this study is completely voluntary and you may refuse to participate or choose to withdraw at any time without penalty or loss of benefits to which you are otherwise entitled.
+          </p><br/>
+          <p>
+            <strong>How to contact the researcher:</strong> f you have questions or
+        concerns about your participation or payment, or want to request a
+        summary of research findings, please contact  <a href="mailto:yzubak@wisc.edu">yzubak@wisc.edu</a>.
+          </p><br/>
+          <p>
+            <strong>Who to contact about your rights in this research:</strong> For
+        questions, concerns, suggestions, or complaints that have not been or
+        cannot be addressed by the researcher, or to report research-related
+        harm, please contact the University of Wisconsin-Madison Human Research Protection Program at 608-890-4399 or <a href="mailto:compliance@research.wisc.edu">compliance@research.wisc.edu</a>.
+          </p><br/>
+        </div>
+        <Button handleClick={next} autoFocus>
+          <p>Next</p>
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ experiment 3 - open chat/client/src/intro-exit/ExitSurvey.jsx
@@ -0,0 +1,292 @@
+import { usePlayer } from "@empirica/core/player/classic/react";
+import React, { useState } from "react";
+import { Alert } from "../components/Alert";
+import { Button } from "../components/Button";
+
+export function ExitSurvey({ next }) {
+  const labelClassName = "block text-sm font-medium text-gray-700 my-2";
+  const inputClassName =
+    "appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-empirica-500 focus:border-empirica-500 sm:text-sm";
+  const player = usePlayer();
+
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [strength, setStrength] = useState("");
+  const [fair, setFair] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [education, setEducation] = useState("");
+  const [teammates, setTeammates] = useState("");
+  const [notes, setNotes] = useState("");
+  const [politics, setPolitics] = useState("");
+  const [geo, setGeo] = useState("");
+  const [ethnicity, setEthnicity] = useState("");
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    player.set("exitSurvey", {
+      age,
+      gender,
+      strength,
+      fair,
+      feedback,
+      education,
+      teammates,
+      notes,
+      politics,
+      geo,
+      ethnicity,
+    });
+    next();
+  }
+
+  function handleEducationChange(e) {
+    setEducation(e.target.value);
+  }
+
+  return (
+    <div className="py-8 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+    
+
+      <form
+        className="mt-12 space-y-8 divide-y divide-gray-200"
+        onSubmit={handleSubmit}
+      >
+        <div className="space-y-8 divide-y divide-gray-200">
+          <div>
+            <div>
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Exit Survey
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">
+                Please answer the following short survey. You do not have to
+                provide any information you feel uncomfortable with.
+              </p>
+            </div> <br/> <br/>
+<h3 className="text-medium leading-6 font-medium text-gray-900">
+                Demographic Information
+              </h3>
+            <div className="space-y-8 mt-6">
+              <div className="flex flex-row">
+                <div>
+                  <label htmlFor="email" className={labelClassName}>
+                    Age
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="age"
+                      name="age"
+                      type="number"
+                      autoComplete="off"
+                      className={inputClassName}
+                      value={age}
+                      onChange={(e) => setAge(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="ml-5">
+                  <label htmlFor="email" className={labelClassName}>
+                    Gender
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="gender"
+                      name="gender"
+                      autoComplete="off"
+                      className={inputClassName}
+                      value={gender}
+                      onChange={(e) => setGender(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="ml-5">
+                  <label htmlFor="email" className={labelClassName}>
+                    Ethnicity
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="ethnicity"
+                      name="ethnicity"
+                      autoComplete="off"
+                      className={inputClassName}
+                      value={ethnicity}
+                      onChange={(e) => setEthnicity(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="ml-5">
+                  <label htmlFor="email" className={labelClassName}>
+                    Political Leaning
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="politics"
+                      name="politics"
+                      autoComplete="off"
+                      className={inputClassName}
+                      value={politics}
+                      onChange={(e) => setPolitics(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="ml-5">
+                  <label htmlFor="email" className={labelClassName}>
+                    Nationality / Area
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="geo"
+                      name="geo"
+                      autoComplete="off"
+                      className={inputClassName}
+                      value={geo}
+                      onChange={(e) => setGeo(e.target.value)}
+                    />
+                  </div>
+                </div>
+                
+              </div>
+
+              <div>
+                <label className={labelClassName}>
+                  Education 
+                </label>
+                <div className="grid gap-2">
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="high-school"
+                    label="High School"
+                    onChange={handleEducationChange}
+                  />
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="bachelor"
+                    label="US Bachelor's Degree"
+                    onChange={handleEducationChange}
+                  />
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="master"
+                    label="Master's or higher"
+                    onChange={handleEducationChange}
+                  />
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="other"
+                    label="Other"
+                    onChange={handleEducationChange}
+                  />
+                </div>
+              </div>
+<h3 className="text-medium leading-6 font-medium text-gray-900">
+                Gameplay
+              </h3>
+              <div className="grid grid-cols-2 gap-x-6 gap-y-3">
+                <label className={labelClassName}>
+                  Did you take notes on what others said?
+                </label>
+
+ <label className={labelClassName}>
+                  Did you find your teammates to be good co-players? Were they telling the truth?
+                </label>
+                <textarea
+                  className={inputClassName}
+                  dir="auto"
+                  id="strength"
+                  name="strength"
+                  rows={4}
+                  value={notes}
+                  onChange={(e) => setNotes(e.target.value)}
+                />
+
+               
+                <textarea
+                  className={inputClassName}
+                  dir="auto"
+                  id="teammates"
+                  name="teammates"
+                  rows={4}
+                  value={teammates}
+                  onChange={(e) => setTeammates(e.target.value)}
+                />
+              </div>
+              <h3 className="text-medium leading-6 font-medium text-gray-900">
+                Review
+              </h3>
+              <div className="grid grid-cols-3 gap-x-6 gap-y-3">
+                <label className={labelClassName}>
+                  How would you describe your strength in the game?
+                </label>
+
+                <label className={labelClassName}>
+                  Do you feel the pay was fair?
+                </label>
+
+                <label className={labelClassName}>
+                  Feedback, including problems you encountered.
+                </label>
+
+               
+
+                <textarea
+                  className={inputClassName}
+                  dir="auto"
+                  id="strength"
+                  name="strength"
+                  rows={4}
+                  value={strength}
+                  onChange={(e) => setStrength(e.target.value)}
+                />
+
+                <textarea
+                  className={inputClassName}
+                  dir="auto"
+                  id="fair"
+                  name="fair"
+                  rows={4}
+                  value={fair}
+                  onChange={(e) => setFair(e.target.value)}
+                />
+
+                <textarea
+                  className={inputClassName}
+                  dir="auto"
+                  id="feedback"
+                  name="feedback"
+                  rows={4}
+                  value={feedback}
+                  onChange={(e) => setFeedback(e.target.value)}
+                />
+                 
+              </div>
+
+              <div className="mb-12">
+                <Button type="submit">Submit</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export function Radio({ selected, name, value, label, onChange }) {
+  return (
+    <label className="text-sm font-medium text-gray-700">
+      <input
+        className="mr-2 shadow-sm sm:text-sm"
+        type="radio"
+        name={name}
+        value={value}
+        checked={selected === value}
+        onChange={onChange}
+      />
+      {label}
+    </label>
+  );
+}
