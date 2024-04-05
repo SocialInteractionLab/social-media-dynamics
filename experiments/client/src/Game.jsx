3c9b6df8069c3532d38b9ea85340eff715c2adc1
zubyul <yulia.zubak@mail.utoronto.ca>
2024-04-05 15:47:27 -0500

    fixed overlay issue

--- experiments/client/src/Game.jsx
+++ experiments/client/src/Game.jsx
@@ -1,9 +1,8 @@
 import { useGame } from "@empirica/core/player/classic/react";
-
 import React from "react";
-import { Chat } from "./components/Chat"
-import { Opinion } from "./components/Opinion"
-import { Profile } from "./components/Profile"
+import { Chat } from "./components/Chat";
+import { Opinion } from "./components/Opinion";
+import { Profile } from "./components/Profile";
 import { World } from "./components/World";
 
 export function Game() {
@@ -18,11 +17,11 @@ export function Game() {
         <div className="h-full w-full flex items-center justify-center">
           <World />
         </div>
-        <div className="h-full w-full flex items-center justify-center">
+        <div className="h-full w-full flex items-center justify-center relative"> 
           <Chat scope={game} attribute="chat" />
         </div>
       </div>
-      <div className="w-full flex  flex items-center justify-center">
+      <div className="w-full flex justify-center">
         <Opinion scope={game} attribute="opinion"/>
       </div>
     </div>
