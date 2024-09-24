0118ce2a57f8c5a2725394b99142796dc007bf85
zubyul <yulia.zubak@mail.utoronto.ca>
2024-09-23 19:11:43 -0700

    working on lobby and random data

---
+++ experiments/client/src/intro-exit/Sorry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Alert } from "../components/Alert";
+
+
+export function Sorry() {
+  
+
+  return (
+   
+      <p>
+        Sorry, it seems the game has crashed or been cancelled! Unfortunately, we do not re-run players within one game, but please submit the following code to receive your compensation:{" "}
+        <strong>NEW CODE</strong>.
+      </p>
+  
+  );
+}
+
