b7a163878edce45ca0fc4d6575d0dd5d67f513b8
zubyul <yulia.zubak@alumni.utoronto.ca>
2025-01-01 15:37:51 -0800

    switch json

--- experiments/server/src/callbacks.js
+++ experiments/server/src/callbacks.js
@@ -4,7 +4,7 @@ import _ from "lodash";
 import fs from 'fs'; // Import fs to read JSON file
 import { getNextGame } from './assignment.js';
 
-  const gamesData = JSON.parse(fs.readFileSync('games.json'));
+  const gamesData = JSON.parse(fs.readFileSync('games_2.json'));
    // console.log(gamesData)
       // Load pre-generated critters from games.json
 
