b942ff5695fea1977458f0d3f2f19b04d4de3ce1
zubyul <yulia.zubak@alumni.utoronto.ca>
2025-01-03 16:08:25 -0800

    improved missing games

--- experiments/server/src/assignment.js
+++ experiments/server/src/assignment.js
@@ -71,7 +71,7 @@ async function getNextGame() {
     try {
         let count = await readCount();
         let increment = true;
-        if (count >= 70) {
+        if (count >= 56) {
             const data = await fs.readFile(filePath, 'utf8');
             const jsonData = JSON.parse(data);
             jsonData.count = 0;
