4bdb2e551ffa075be3d721a3becdac3ce4b13f97
zubyul <yulia.zubak@alumni.utoronto.ca>
2025-01-09 19:34:48 -0800

    last set of games

--- experiments/server/src/assignment.js
+++ experiments/server/src/assignment.js
@@ -71,7 +71,7 @@ async function getNextGame() {
     try {
         let count = await readCount();
         let increment = true;
-        if (count >= 41) {
+        if (count >= 8) {
             const data = await fs.readFile(filePath, 'utf8');
             const jsonData = JSON.parse(data);
             jsonData.count = 0;
