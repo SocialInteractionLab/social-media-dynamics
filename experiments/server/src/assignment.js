d7aa62250fc6f6cdfdcefff59e706f2efe79baa0
zubyul <yulia.zubak@alumni.utoronto.ca>
2025-01-11 14:28:04 -0800

    crashed games rerun

--- experiments/server/src/assignment.js
+++ experiments/server/src/assignment.js
@@ -71,7 +71,7 @@ async function getNextGame() {
     try {
         let count = await readCount();
         let increment = true;
-        if (count >= 38) {
+        if (count >= 9) {
             const data = await fs.readFile(filePath, 'utf8');
             const jsonData = JSON.parse(data);
             jsonData.count = 0;
